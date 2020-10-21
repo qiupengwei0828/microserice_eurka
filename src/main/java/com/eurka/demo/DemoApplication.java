62205b6ec6f565f14ec868ffae8689cae90e7cfe
仇鹏伟 <1940519031@qq.com>
2020-10-21 15:03:12 +0800

    增加服务发现配置

--- src/main/java/com/eurka/demo/DemoApplication.java
+++ src/main/java/com/eurka/demo/DemoApplication.java
@@ -3,7 +3,10 @@ package com.eurka.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+/***
+ * 服务发现类
+ * @author Administrator
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class DemoApplication {
